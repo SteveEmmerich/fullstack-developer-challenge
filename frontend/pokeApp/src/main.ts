8f8d4f19f65ac9601807319739830808b6040e58
Steve Emmerich <sdemmer@Steves-MacBook-Pro.local>
2019-11-25 14:03:03 -0500

    installed amplify

--- frontend/pokeApp/src/main.ts
+++ frontend/pokeApp/src/main.ts
@@ -4,6 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import awsconfig from './aws-exports';
+import Auth from '@aws-amplify/auth';
+
+Auth.configure(awsconfig);
+
+
 if (environment.production) {
   enableProdMode();
 }
